36b3163de38d2a2b5e1507096e8774e6352e6456
BWOil <115447698+BWOil@users.noreply.github.com>
2024-01-12 22:31:54 +1100

    extract deposit() and withdraw() in menu.cs
    - updated AccountUtilities
    - add Enum Transactiontype to menu.cs
    - extract deposit() and withdraw()  in to only one method called " ProcessTransaction()"

--- Assignment1/Assignment1/Utilities/AccountUtilities.cs
+++ Assignment1/Assignment1/Utilities/AccountUtilities.cs
@@ -1,5 +1,6 @@
 ﻿using Assignment1.Manager;
 using Assignment1.Models;
+using static Assignment1.Menu;
 
 namespace Assignment1.Utilities
 {
@@ -11,19 +12,23 @@ namespace Assignment1.Utilities
             Console.WriteLine($"Account Number: {account.AccountNumber}\nAccount Type: {accountType}\nBalance: ${account.Balance:F2}\n");
         }
 
-        public static void PerformTransaction(AccountManager accountManager, Account account, decimal amount, string comment, bool isDeposit)
+        public static void PerformTransaction(AccountManager accountManager, Account account, decimal amount, string comment, TransactionType transactionType)
         {
-            if (isDeposit)
+            switch (transactionType)
             {
-                accountManager.Deposit(account, amount, comment);
-            }
-            else
-            {
-                if (amount > account.Balance)
-                {
-                    throw new InvalidOperationException("Insufficient funds for withdrawal.");
-                }
-                accountManager.Withdraw(account, amount, comment);
+                case TransactionType.Deposit:
+                    accountManager.Deposit(account, amount, comment);
+                    break;
+                case TransactionType.Withdraw:
+                    if (amount > account.Balance)
+                    {
+                        throw new InvalidOperationException("Insufficient funds for withdrawal.");
+                    }
+                    accountManager.Withdraw(account, amount, comment);
+                    break;
+                // Add cases for Transfer and ServiceCharge if needed
+                default:
+                    throw new InvalidOperationException("Invalid transaction type.");
             }
         }
     }
