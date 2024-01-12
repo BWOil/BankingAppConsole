c190b9a5661d1593aacbad723b356e1fa79a6220
BWOil <115447698+BWOil@users.noreply.github.com>
2024-01-12 22:23:22 +1100

    update menu.cs

---
+++ Assignment1/Assignment1/Utilities/AccountUtilities.cs
@@ -0,0 +1,30 @@
+﻿using Assignment1.Manager;
+using Assignment1.Models;
+
+namespace Assignment1.Utilities
+{
+    public static class AccountUtilities
+    {
+        public static void PrintAccountDetails(Account account)
+        {
+            string accountType = account.AccountType == "S" ? "Saving" : (account.AccountType == "C" ? "Checking" : "Unknown");
+            Console.WriteLine($"Account Number: {account.AccountNumber}\nAccount Type: {accountType}\nBalance: ${account.Balance:F2}\n");
+        }
+
+        public static void PerformTransaction(AccountManager accountManager, Account account, decimal amount, string comment, bool isDeposit)
+        {
+            if (isDeposit)
+            {
+                accountManager.Deposit(account, amount, comment);
+            }
+            else
+            {
+                if (amount > account.Balance)
+                {
+                    throw new InvalidOperationException("Insufficient funds for withdrawal.");
+                }
+                accountManager.Withdraw(account, amount, comment);
+            }
+        }
+    }
+}
