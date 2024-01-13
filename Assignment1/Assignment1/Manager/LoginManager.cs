b08706d901f387a2f897bcd2fa8b71092966d25b
Shina Qin <bqshina@gmail.com>
2024-01-13 12:48:00 +1100

    fix loginID issue

--- Assignment1/Assignment1/Manager/LoginManager.cs
+++ Assignment1/Assignment1/Manager/LoginManager.cs
@@ -15,7 +15,7 @@ namespace Assignment1.Manager
 			_connectionString = connectionString;
 		}
 
-		public List<Login> GetLogin(int customerID)
+		public List<Login> GetLogin()
 		{
 			using var connection = new SqlConnection(_connectionString);
         
@@ -23,12 +23,29 @@ namespace Assignment1.Manager
             using var command = connection.CreateCommand();
             command.CommandText = "select * from Login";
 
+            return ReturnList(command);
+        }   
+
+        public List<Login> GetLoginByCustomerID(int customerID)
+        {
+            using var connection = new SqlConnection(_connectionString);
+
+
+            using var command = connection.CreateCommand();
+            command.CommandText = "select * from Login where CustomerID = @customerID";
+            command.Parameters.AddWithValue("customerID", customerID);
+
+            return ReturnList(command);
+        }
+
+        private List<Login> ReturnList(SqlCommand command)
+        {
             return command.GetDataTable().Select().Select(x => new Login
             {
                 LoginID = x.Field<string>("LoginID"),
-                CustomerID = customerID,
+                CustomerID = x.Field<int>("CustomerID"),
                 PasswordHash = x.Field<string>("PasswordHash")
-               
+
             }).ToList();
         }
 
